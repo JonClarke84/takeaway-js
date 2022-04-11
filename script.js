eb7c2fed857785d2c0d2bda3ec269aad91b3b0ee
Jon Clarke <jonclarke1984@googlemail.com>
2022-04-11 16:58:07 +0100

    add items menu button is working

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const menuItems = document.getElementById('menu-items')
 
 const menuList = [
-  {item: 'Curry', price: 8.99},
-  {item: 'Chicken', price: 9.99},
-  {item: 'Beef', price: 19.99},
-  {item: 'Fish', price: 10.99},
-  {item: 'Pork', price: 10.99},
-  {item: 'Vegetarian Curry', price: 8.99},
-  {item: 'Vegan Pizza', price: 11.99}
+  {name: 'Curry', price: 8.99, quantity: 0},
+  {name: 'Chicken', price: 9.99, quantity: 0},
+  {name: 'Beef', price: 19.99, quantity: 0},
+  {name: 'Fish', price: 10.99, quantity: 0},
+  {name: 'Pork', price: 10.99, quantity: 0},
+  {name: 'Vegetarian Curry', price: 8.99, quantity: 0},
+  {name: 'Vegan Pizza', price: 11.99, quantity: 0},
 ]
 
 function displayMenu (menuList) {
   menuList.forEach(item => {
     menuItems.innerHTML += `<tr>
-                        <td>${item.item}</td>
+                        <td>${item.name}</td>
                         <td>£${item.price}</td>
+                        <form name="${item.name}-button">
+                        <td><input type="number" value="${item.quantity}" min="0" /></td>
+                        <td><button class="add-item" id="${item.name}-button" onclick="buttonClick()">Add Item</button></td>
+                        </form>
                       </tr>`
   })
   menuItems.innerHTML += `</table>`
 }
 
+
+function buttonClick() {
+  const form = event.target.parentNode.parentNode
+  const quantity = parseInt(form.querySelector('input').value)
+  const input = form.querySelector('input')
+  input.value = quantity + 1
+  console.log(input.value)
+}
 displayMenu(menuList)
-console.log(menuList)
