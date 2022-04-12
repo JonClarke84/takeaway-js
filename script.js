c91bf25401825ec6844cab1c0f41b6534aaec69b
Jon Clarke <jonclarke1984@googlemail.com>
2022-04-12 11:56:51 +0100

    total price box now working

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const menuItems = document.getElementById('menu-items')
-const totalPrice = document.getElementById('total-price')
+let totalPrice = document.getElementById('total-price')
 
 const menuList = [
-  {name: 'Curry', price: 8.99, quantity: 0},
-  {name: 'Chicken', price: 9.99, quantity: 0},
-  {name: 'Beef', price: 19.99, quantity: 0},
-  {name: 'Fish', price: 10.99, quantity: 0},
-  {name: 'Pork', price: 10.99, quantity: 0},
-  {name: 'Vegetarian Curry', price: 8.99, quantity: 0},
-  {name: 'Vegan Pizza', price: 11.99, quantity: 0},
+  {name: 'Curry', price: 9, quantity: 0},
+  {name: 'Chicken', price: 10, quantity: 0},
+  {name: 'Beef', price: 20, quantity: 0},
+  {name: 'Fish', price: 11, quantity: 0},
+  {name: 'Pork', price: 12, quantity: 0},
+  {name: 'Vegetarian Curry', price: 9, quantity: 0},
+  {name: 'Vegan Pizza', price: 12, quantity: 0},
 ]
 
 function displayMenu (menuList) {
@@ -17,7 +17,7 @@ function displayMenu (menuList) {
                         <td>${item.name}</td>
                         <td class="price">£${item.price}</td>
                         <form name="${item.name}-button">
-                        <td><input type="number" value="${item.quantity}" min="0" /></td>
+                        <td><input type="number" name="total-item-value" value="${item.quantity}" min="0" /></td>
                         <td><button class="add-item" id="${item.name}-button" onclick="buttonClick()">Add Item</button></td>
                         </form>
                       </tr>`
@@ -28,13 +28,16 @@ function convertPriceToFloat (price) {
   return parseFloat(price.replace('£', ''))
 }
 
+let orderTotal = 0.00
+
 function buttonClick() {
   const form = event.target.parentNode.parentNode
-  const quantity = parseInt(form.querySelector('input').value)
+  const quantity = parseInt(form.querySelector('input').value) + 1
   const price = convertPriceToFloat(form.parentNode.querySelector('.price').innerText)
   const input = form.querySelector('input')
-  input.value = quantity + 1
-  totalPrice.innerText = `Total Price: £${(price * (quantity + 1)).toFixed(2)}`
+  input.value = quantity
+  orderTotal += parseFloat(price.toFixed(2))
+  totalPrice.value = `${orderTotal.toFixed(2)}`
 }
 
 displayMenu(menuList)
